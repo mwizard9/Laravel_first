0ce0eb316a595f3ca5e38315f46760611069839f
mwizard9 <manishhimalaya64@gmail.com>
2022-07-04 07:36:25 +0545

    model_create

--- database/migrations/2022_06_30_022934_create_students_table.php
+++ database/migrations/2022_06_30_022934_create_students_table.php
@@ -16,6 +16,17 @@ class CreateStudentsTable extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->string('name',150);
+            $table->string('mobile',10)->unique();
+            $table->string('citizenship',20)->unique();
+            $table->string('gender',1);
+            $table->string('blood_group',3);
+            $table->string('perm_address')->nullable;
+            $table->date('dob');
+            $table->boolean('is_active');
+            $table->boolean('is_alumuni');
+            $table->string('picture')->nullable;
+
         });
     }
 
