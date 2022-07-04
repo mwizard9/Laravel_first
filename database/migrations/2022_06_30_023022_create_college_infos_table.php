0e94d81a687eb4710e08790255594d31a6f2bc9a
mwizard9 <manishhimalaya64@gmail.com>
2022-07-04 20:06:33 +0545

    college_info model

--- database/migrations/2022_06_30_023022_create_college_infos_table.php
+++ database/migrations/2022_06_30_023022_create_college_infos_table.php
@@ -16,6 +16,12 @@ class CreateCollegeInfosTable extends Migration
         Schema::create('college_infos', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->string('faculty');
+            $table->string('batch_id');
+            $table->string('semester_id');
+            $table->string('tu_reg.no');
+            $table->string('symbol.no');
+           
         });
     }
 
