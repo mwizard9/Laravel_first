807254b3693712a611af7fbe64cca9fda086b751
mwizard9 <manishhimalaya64@gmail.com>
2022-07-06 18:06:05 +0545

    form

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -40,18 +40,7 @@
                     <label for="exampleInputPassword1">citizenship number</label>
                     <input type="string" name="citizenship" class="form-control" id="exampleInputPassword1" placeholder="citizenship number">
                   </div>
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                    </div>
-                  </div>
+                  
                   <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1">Check me out</label>
