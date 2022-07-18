1c4833b2563806c8726cbe3cefbf78594c10044d
mwizard9 <manishhimalaya64@gmail.com>
2022-07-18 21:37:48 +0545

    adding <a> tag

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -1,61 +1,81 @@
-
 @extends('welcome')
 @section('main_content')
-    <div class="row">
-        <div class="col-md-6">
-            <!-- general form elements -->
-            <div class="card card-primary">
-                <div class="card-header">
-                    <h3 class="card-title">Quick Example</h3>
-                </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form method="POST" action="{{ route('student.store') }}">
-                @csrf
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="Email">Email address</label>
-                            <input type="email" name="email" class="form-control" id="Email" placeholder="Enter email">
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" name="name"
-                                   placeholder="Name">
-                        </div>
-                        <div class="form-group">
-                            <label for="dob">Date of birth</label>
-                            <input type="text" class="form-control" name="dob"
-                                   placeholder="dob">
-
-
-                               
-                        </div>
-
-                        <div class="form-check">
-                        <label for="Mobile">Mobile</label>
-                        <input type="text" class="form-control" name="mobile"
-                                   placeholder="Number">
-
-                            
-                        </div>
-
-                        <div class="form-check">
-                        <label for="gender">Gender</label>
-                        <input type="text" class="form-control" name="gender"
-                                   placeholder="0 for male, 1 for female">
-
-                            
-                        </div>
-
-                    </div>
-                    <!-- /.card-body -->
-
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
+          <!-- left column -->
+    <div class="col-md-6">
+        <!-- general form elements -->
+        <div class="card card-primary">
+            <div class="card-header">
+            <h3 class="card-title">Enter Your Details</h3>
             </div>
-            <!-- /.card -->
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form method="post" action="{{ route('students.store') }}">
+            @csrf <!--  cross origin side reference -->
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input name="name" type="text" class="form-control" id="name" placeholder="Enter Full Name">
+                </div>
+                <div class="form-group">
+                    <label for="mobile">Mobile</label>
+                    <input name="mobile" type="text" class="form-control" id="mobile" placeholder="Enter Mobile No.">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input name="email" type="text" class="form-control" id="email" placeholder="Enter Email">
+                </div>
+                <div class="form-group">
+                    <label for="citizenship">Citizenship</label>
+                    <input name="citizenship" type="text" class="form-control" id="citizenship" placeholder="Enter Citizenship">
+                </div>
+                <div class="form-group">
+                    <label for="gender">Gender</label>
+                    <input name="gender" type="text" class="form-control" id="gender" placeholder="Enter Gender">
+                </div>
+                <div class="form-group">
+                    <label for="blood_group">Blood Group</label>
+                    <input name="blood_group" type="text" class="form-control" id="blood_group" placeholder="Enter blood group">
+                </div>
+                <div class="form-group">
+                    <label for="perm_address">Permanent Address</label>
+                    <input name="perm_address" type="text" class="form-control" id="perm_address" placeholder="Enter Permanent Address">
+                </div>
+                <div class="form-group">
+                    <label for="temp_address">Temporary Address</label>
+                    <input name="temp_address" type="text" class="form-control" id="temp_address" placeholder="Enter Temporary Address">
+                </div>
+                <div class="form-group">
+                    <label for="dob">Dob</label>
+                    <input name="dob" type="text" class="form-control" id="dob" placeholder="Enter Dob">
+                </div>
+                <div class="form-group">
+                    <label for="is_active">Is Active?</label>
+                    <input name="is_active" type="checkbox" class="form-control" id="is_active">
+                </div>
+                <div class="form-group">
+                    <label for="is_almuni">Is Almuni?</label>
+                    <input name="is_almuni" type="checkbox" class="form-control" id="is_almuni">
+                </div>
+                <div class="form-group">
+                    <label for="picture">Choose an image:</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="picture">
+                        <label class="custom-file-label" for="picture">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+            </form>
         </div>
     </div>
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,11 @@
   <title>AdminLTE 3 | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback')}}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{asset('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
   <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
@@ -213,27 +213,33 @@
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Dashboard
+                Administrator
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
+                <a href="{{ route('student.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
+                  <p>Student Management</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
+                <a href="{{ route('faculty.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
+                  <p>Faculty Managment</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="{{ route('semester.index')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                  <p>Semester Management</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('batch.index')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Batch Management</p>
                 </a>
               </li>
             </ul>
@@ -850,17 +856,13 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        @yield('header')
-      </div><!-- /.container-fluid -->
-    </div>
+    @yield('content_header')
     <!-- /.content-header -->
-
+    <br><br>
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-       @yield('main_content')
+        @yield('main_content')
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
@@ -900,7 +902,7 @@
 <script src="{{asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script>
 <script src="{{asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
 <!-- jQuery Knob Chart -->
-<script src="ugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
 <script src="{{asset('plugins/moment/moment.min.js')}}"></script>
 <script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
@@ -912,8 +914,6 @@
 <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('dist/js/adminlte.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{asset('dist/js/demo.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
 </body>
